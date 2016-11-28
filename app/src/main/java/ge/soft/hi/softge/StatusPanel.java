76df23206a3c8d8655f72e3e7748453ed259d238
kakha <kakha@soft.ge>
2016-11-28 16:46:20 +0400

    comment recycler

---
+++ app/src/main/java/ge/soft/hi/softge/StatusPanel.java
@@ -0,0 +1,26 @@
+package ge.soft.hi.softge;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * Created by kakha on 11/28/16.
+ */
+
+public class StatusPanel extends TextView {
+
+    public StatusPanel(Context context) {
+        super(context);
+        setStatus(null);
+    }
+
+    public StatusPanel(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setStatus(null);
+    }
+
+    private void setStatus(String date){
+        setText("New");
+    }
+}
